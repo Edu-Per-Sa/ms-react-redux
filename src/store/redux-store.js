d3b97a7b07ec5f3e07f03ccb4d89dce6568caedb
Luis Peraza <leperazas@gmail.com>
2025-05-07 11:39:25 +0200

    Cambiado el redux por redux-Toolkit

--- src/store/redux-store.js
+++ src/store/redux-store.js
@@ -1,26 +1,29 @@
-import { createStore } from 'redux';
 
-const initialState = {
-    counter: 0,
-}
+import { configureStore } from '@reduxjs/toolkit';
 
-const storeReducerFunction = (actualState = initialState, actions) => {
+import counterSliceReducer from './counter-slice';
 
-    if(actions.type === 'increment') {
-        return {
-            counter: actualState.counter + 1,
-        };
-    }
+const store = configureStore({
+    reducer: {counter: counterSliceReducer}
+})
+
+// const storeReducerFunction = (actualState = initialState, actions) => {
+
+//     if(actions.type === 'increment') {
+//         return {
+//             counter: actualState.counter + 1,
+//         };
+//     }
     
-    if(actions.type === 'decrement') {
-        return {
-            counter: actualState.counter - 1,
-        };
-    }
+//     if(actions.type === 'decrement') {
+//         return {
+//             counter: actualState.counter - 1,
+//         };
+//     }
 
-    return actualState;
-};
+//     return actualState;
+// };
 
-const store = createStore(storeReducerFunction);
+// const store = createStore(storeReducerFunction);
 
 export default store;
